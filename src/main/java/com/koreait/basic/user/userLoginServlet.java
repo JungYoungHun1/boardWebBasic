b39a6d8ee10ebe2b3aa5307d1c3a571d5bce8c7c
younghun <ju39001@naver.com>
2021-12-01 16:53:28 +0900

    Initial commit

---
+++ src/main/java/com/koreait/basic/user/userLoginServlet.java
@@ -0,0 +1,24 @@
+package com.koreait.basic.user;
+
+import com.koreait.basic.Utils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class userLoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Utils.displayView("로그인","user/login", req, res);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+}
